000137f548ec5ee29fb5d46e1a044f39f210fc74
Babita Ratudi <babita.ratudi@publicissapient.com>
2022-09-03 19:04:37 +0530

    first commit

---
+++ src/main/java/com/example/jenkindockerintegration/JenkinDockerIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkindockerintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinDockerIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinDockerIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkindockerintegration/JenkinDockerIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkindockerintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinDockerIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
